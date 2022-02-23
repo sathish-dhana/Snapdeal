e20c8c156d98e6dd7486f848b63786baf1a0a267
sathish-dhana <cosmiccoincedenceinvoid@gmail.com>
2022-02-23 08:14:31 +0530

    day-1

---
+++ components/navBar.js
@@ -0,0 +1,42 @@
+function navbar() {
+    return `<div id="navTop">
+    <div>
+        Brand Waali Quality, Bazaar Waali Deal!
+    </div>
+    <div>
+        Deliveries to more than 2500 towns and cities and expanding.
+    </div>
+</div>
+<div id="navBar">
+    <div id="logo">
+        <img src="logo1.png">
+    </div>
+    <div id="search">
+        <div id="pack">
+            <input id="searchBox" placeholder="Search products & brands">
+            <div id="button">
+                <div>
+                    <span class="fas fa-search"> </span>
+                    Search
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="rightNav">
+        <div id="cartDiv">
+            <div>
+                Cart
+                <span style="font-size: 20px;" class="fas fa-cart-plus"> </span>
+            </div>
+        </div>
+        <div id="profileDiv">
+            <div>
+                <span id="user">Profile&nbsp;</span>
+                <span id="userIcon" style="font-size: 18px;" class="fa-solid fa-user"> </span>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+}
+export default navbar;
