d831c1a7aa20f83cbea006262a811f7b9ed36b01
Sanjayan <97502651+Sanjayan-Ganesan@users.noreply.github.com>
2022-02-27 15:21:19 +0530

    sanjayan
    sanjayan_product_done...

---
+++ components/ethnic_wear_product.js
@@ -0,0 +1,324 @@
+var ethnic = [
+    {
+        image_url:"https://n3.sdlcdn.com/imgs/j/x/w/Alexplum-Red-Bhagalpuri-Silk-Saree-SDL491206765-1-20aaa.jpeg",
+        name: "Alexplum Red Bhagalpuri Silk Saree -",
+        strikedoffprice: 749 ,
+        price: 349,
+        discount:56,
+        category: "saree",
+        star_and_stock: "★★★★★ 4.2(107)",
+    },
+    {
+        image_url:"https://n1.sdlcdn.com/imgs/j/w/1/Bhakarwadi-Blue-Cotton-Silk-Saree-SDL289141087-1-2b204.jpg",
+        name: "Bhakarwadi Blue Cotton Silk Saree -",
+        strikedoffprice: 1599 ,
+        price: 339,
+        discount:86,
+        category: "saree",
+        star_and_stock: "★★★★★ 4.2(107)",
+    },
+    {
+        image_url:"https://n3.sdlcdn.com/imgs/k/b/d/Hritika-Black-Georgette-Saree-Single-SDL603752999-1-174b8.jpg",
+        name: "Hritika Black Georgette Saree - Single",
+        strikedoffprice: 1599 ,
+        price: 299,
+        discount:36,
+        category: "saree"
+    },
+    {
+        image_url:"https://n1.sdlcdn.com/imgs/k/b/9/Darshita-International-Beige-Silk-Saree-SDL872839995-1-1f55f.jpg",
+        name: "Darshita International Beige Silk Saree - Single",
+        strikedoffprice: 4999 ,
+        price: 1499,
+        discount:26,
+        category: "saree"
+    },
+    {
+        image_url:"https://n4.sdlcdn.com/imgs/k/d/n/Darshita-International-Grey-Cotton-Silk-SDL547935113-1-c15c5.jpg",
+        name: "Darshita International Grey Cotton Silk Saree - Single",
+        strikedoffprice: 2999 ,
+        price: 799,
+        discount:23,
+        category: "saree"
+    },
+    {
+        image_url:"https://n1.sdlcdn.com/imgs/j/v/j/Bhakarwadi-Blue-Georgette-Saree-Single-SDL094232966-1-9710f.jpg",
+        name: "Bhakarwadi Blue Georgette Printed Casual Wear Saree",
+        strikedoffprice: 1599 ,
+        price: 309,
+        discount:50,
+        category: "saree"
+    },
+    {
+        image_url:"https://n2.sdlcdn.com/imgs/g/6/0/Aika-Yellow-Net-Semi-Stitched-SDL384281829-1-31b13.jpg",
+        name: "Aika Yellow and Blue Net Semi Stitched Lehenga",
+        strikedoffprice: 1499 ,
+        price: 477,
+        discount:88,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n1.sdlcdn.com/imgs/j/r/d/Zistatva-Black-Velvet-Chaniya-Choli-SDL584357863-1-dd595.jpeg",
+        name: "Zistatva Black Velvet Chaniya Choli Semi Stitched Lehenga",
+        strikedoffprice: 2999 ,
+        price: 1499,
+        discount:26,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n1.sdlcdn.com/imgs/e/q/3/Aika-Multicoloured-Bangalore-Silk-Circular-SDL087395892-1-4dafd.jpg",
+        name: "Aika Red and Beige Bangalore Silk Circular Semi Stitched Lehenga",
+        strikedoffprice: 1499 ,
+        price: 453,
+        discount:13,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n2.sdlcdn.com/imgs/k/c/n/Sherine-Yellow-Cotton-Silk-Chaniya-SDL187193111-1-5d65d.jpg",
+        name: "Sherine Yellow Cotton Silk Chaniya Choli Stitched Lehenga Single",
+        strikedoffprice: 3999 ,
+        price: 845,
+        discount:59,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n4.sdlcdn.com/imgs/j/m/t/NIKLU-FASHION-Red-Silk-Chaniya-SDL457189112-1-a006f.jpeg",
+        name: "NIKLU FASHION Red Silk Chaniya Choli Semi Stitched Lehenga",
+        strikedoffprice: 4999 ,
+        price: 775,
+        discount:06,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n4.sdlcdn.com/imgs/j/u/a/Darshita-International-Blue-Banarasi-Silk-SDL802275400-1-51bb7.jpg",
+        name: "Darshita International Blue Banarasi Silk Chaniya Choli Unstitched Lehenga Single",
+        strikedoffprice: 3999 ,
+        price: 756,
+        discount:16,
+        category:"lehenga"
+      },
+      {
+        image_url:"https://n1.sdlcdn.com/imgs/g/i/l/Aika-Blue-Bhagalpuri-Silk-Anarkali-SDL230272148-1-9c39e.jpeg",
+        name: "Aika Blue and Purple Bhagalpuri Silk Anarkali Semi-Stitched Suit",
+        strikedoffprice: 1599 ,
+        price: 520,
+        discount:54,
+        category: "kurta"
+
+},
+    {
+        image_url:"https://n4.sdlcdn.com/imgs/j/s/1/Panihari-White-Cotton-Unstitched-Dress-SDL859259320-1-26ffc.jpeg",
+        name: "Panihari White Cotton Unstitched Dress Material",
+        strikedoffprice: 1399 ,
+        price: 435,
+        discount:16,
+        category: "kurta",
+        star_and_stock: "★★★★ 3.8(107)",
+    },
+    {
+        image_url:"https://n3.sdlcdn.com/imgs/j/d/w/shree-jeenmata-collection-Brown-Crepe-SDL263605591-1-47846.jpg",
+        name: "shree jeenmata collection Brown Crepe Dress Material",
+        strikedoffprice: 1499 ,
+        price: 450,
+        discount:76,
+        category: "kurta",
+        star_and_stock: "★★★★★ 4.8(107)",
+    },
+    {
+        image_url:"https://n2.sdlcdn.com/imgs/j/p/b/Maroosh-Pink-Cotton-Silk-Straight-SDL416553658-1-c8aa6.jpg",
+        name: "Maroosh Pink Cotton Silk Straight Semi-Stitched Suit",
+        strikedoffprice: 1655 ,
+        price: 654,
+        discount:16,
+        discount:46,
+        category: "kurta"
+    },
+    {
+        image_url:"https://n2.sdlcdn.com/imgs/k/c/8/A-TEX-INDIA-Rayon-Kurti-SDL527395435-1-7843f.jpeg",
+        name: "A - TEX INDIA Rayon Kurti With Pants - Stitched Suit Single",
+        strikedoffprice: 1499 ,
+        price: 699,
+        discount:56,
+        category: "kurta"
+    },
+    {
+        image_url:"https://n1.sdlcdn.com/imgs/j/r/s/Sequin-Crafts-Cotton-Single-Harem-SDL788367040-1-ce426.jpg",
+        name: "Sequin Crafts Cotton Single Harem Pants",
+        strikedoffprice: 1299,
+        price: 455,
+        discount:53,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★ 2.8(107)",
+      },
+      {
+        image_url:"https://n2.sdlcdn.com/imgs/j/t/a/PT-LEGGINGS-Satin-Pack-of-SDL475318990-1-07a05.jpg",
+        name: "PT LEGGINGS Satin Pack of 2 Leggings",
+        strikedoffprice: 1699,
+        price: 755,
+        discount:26,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★ 3.8(107)",
+      },
+      {
+        image_url:"https://n4.sdlcdn.com/imgs/j/b/0/avinzo-Cotton-Single-Semi-Patiala-SDL996056702-4-3a532.jpg",
+        name: "avinzo Cotton Single Semi Patiala Salwar",
+        strikedoffprice: 1699,
+        price: 259,
+        discount:86,
+        category:"Salwars_Leggings"
+      },
+      {
+        image_url:"https://n1.sdlcdn.com/imgs/j/r/u/Sequin-Crafts-Cotton-Single-Harem-SDL175107354-1-d2c41.jpg",
+        name: "Sequin Crafts Cotton Single Harem Pants",
+        strikedoffprice: 999,
+        price: 329,
+        discount:16,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★★ 4.2(107)",
+      },
+      {
+        image_url:"https://n3.sdlcdn.com/imgs/j/p/e/shri-hub-Cotton-Single-Salwar-SDL326916172-1-2df87.jpeg",
+        name: "shri hub Cotton Single Salwar",
+        strikedoffprice: 599,
+        price: 239,
+        discount:65,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★★ 4.2(107)",
+      },
+      {
+        image_url:"https://n1.sdlcdn.com/imgs/j/u/l/avinzo-Cotton-Single-Semi-Patiala-SDL604129611-1-68b79.jpeg",
+        name: "avinzo Cotton Single Semi Patiala Salwar",
+        strikedoffprice: 769,
+        price: 349,
+        discount:68,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★ 3.8(107)",
+      },
+      {
+        image_url:"https://n2.sdlcdn.com/imgs/j/x/u/ANADI-Cotton-Leggings-Single-SDL216972630-1-0ff8e.png",
+        name: "ANADI Cotton Leggings - Single",
+        strikedoffprice: 999,
+        price: 209,
+        discount:90,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★★ 4.4(107)",
+      },
+      {
+        image_url:"https://n3.sdlcdn.com/imgs/j/g/0/Robin-Romeo-Viscose-Single-Leggings-SDL900523992-1-56e96.jpeg",
+        name: "Robin Romeo Viscose Single Leggings",
+        strikedoffprice: 999,
+        price: 289,
+        discount:16,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★★ 4.6(107)",
+      },
+      {
+        image_url:"https://n4.sdlcdn.com/imgs/j/w/k/HomeShop-Fashion-Polyester-Leggings-Single-SDL514324002-1-43ea2.jpeg",
+        name: "HomeShop.Fashion Polyester Leggings - Single",
+        strikedoffprice: 999,
+        price: 289,
+        discount:86,
+        category:"Salwars_Leggings",
+        star_and_stock: "★★★★★ 4.8(107)",
+        
+      },
+]
+
+ localStorage.setItem("ethnic",JSON.stringify(ethnic));
+
+  var container_1 = document.getElementById("product");
+ display_data(ethnic);
+
+function display_data(ethnic){
+  
+    ethnic.map(function(elem){
+
+    var div_pro = document.createElement("div");
+div_pro.style.backgroundColor="white";
+div_pro.style.height = "360px";
+div_pro.style.borderRadius = "10px";
+
+
+//div_pro.style.border = '2px solid white';
+//div_pro.style.hover = ""
+div_pro.id= "div_product";
+//div_pro.setAttribute("id","div_product");
+//div_product.style
+
+
+
+
+
+var image = document.createElement("img");
+image.setAttribute("id", "img1");
+ image.setAttribute('src',elem.image_url);
+
+var title = document.createElement("p");
+title.textContent = elem.name;
+title.style.color = "grey";
+
+
+var strike = document.createElement("p");
+strike.textContent =  "Rs." + elem.strikedoffprice;
+strike.style.textDecoration = "line-through";
+strike.style.color = "grey";
+
+
+var price = document.createElement("h4");
+price.textContent = "Rs."+elem.price;
+price.style.marginTop = "-14%";
+price.style.marginLeft = "30%";
+
+
+var discount_1 = document.createElement("h5");
+discount_1.textContent = elem.discount+"% Off";
+discount_1.style.marginTop = "-17%";
+discount_1.style.marginLeft = "75%";
+discount_1.style.color = "grey";
+
+var star = document.createElement("p");
+star.textContent = elem.star_and_stock;
+star.style.color = "#ffff00"
+star.style.marginTop = "-5%";
+
+div_pro.append(image,title,strike,price,discount_1,star);
+container_1.append(div_pro);
+   });
+  }
+
+
+
+
+
+  function pricesort(){
+    document.getElementById("product").innerHTML = "";
+    var selected = document.getElementById("sort").value;
+    //console.log(selected);
+  
+    if(selected == "Price High To Low"){
+        ethnic.sort(function(a,b){
+    return b.price-a.price;
+  });
+    }
+    if(selected == "Price Low To High"){
+        ethnic.sort(function(a,b){
+    return a.price-b.price;
+  });
+    }
+    if(selected == "Discount"){
+ethnic.sort(function(a,b){
+return b.discount - a.discount;
+});
+    }
+    console.log(ethnic);
+  display_data(ethnic);
+  }
+
+
+
+
+
+  document.getElementById("div_product").addEventListener("click",function(){
+    localStorage.setItem("Bagitems",JSON.stringify(ethnic))
+    window.location.href='cart.html'
+  });
